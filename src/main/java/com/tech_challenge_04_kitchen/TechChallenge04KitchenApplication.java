fe6bd79f007c62664ccc77e91a4f5f281457a67c
Lucas Coelho dos Santos <lucas.goleiro1995@gmail.com>
2024-06-16 11:19:28 -0300

    fisrt commit

---
+++ src/main/java/com/tech_challenge_04_kitchen/TechChallenge04KitchenApplication.java
@@ -0,0 +1,13 @@
+package com.tech_challenge_04_kitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallenge04KitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallenge04KitchenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech_challenge_04_kitchen/TechChallenge04KitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_challenge_04_kitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallenge04KitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
